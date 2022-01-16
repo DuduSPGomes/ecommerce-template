3bf6988f312bc787acbf2571a89314eaaded9e66
Cadu <dudu.spgomes@gmail.com>
2022-01-15 23:00:21 -0300

    feat: product card  component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { Box } from '@chakra-ui/react'
+import ProductList from 'presentation/components/organisms/product-list'
 import React from 'react'
 
 export default function Home() {
-  return <Box bgColor="red">Hello!</Box>
+  return <ProductList />
 }
